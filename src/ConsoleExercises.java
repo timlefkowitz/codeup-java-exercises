bddeb26437b4b30a7db51da29c21d45be4958280
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-04-26 10:36:34 -0500

    Feat: Updates

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -7,9 +7,30 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.println("The value of pi is approximately "+ pi);
 
+        // Enter a number
+
         System.out.println("Enter a integer");
         String intnumber = sc.next();
         System.out.printf("Your number is, %s\n", intnumber);
+
+        // 3 words
+
+        Scanner firstword = new Scanner(System.in);
+        System.out.println("Enter 3 words, Your first word is?");
+        String first = firstword.nextLine();
+
+        Scanner secondword = new Scanner(System.in);
+        System.out.println("Your 2nd word is? ");
+        String second = secondword.nextLine();
+
+        Scanner thirdword = new Scanner(System.in);
+        System.out.println("Your 3rd word is? ");
+        String third = thirdword.nextLine();
+
+        System.out.printf(first, second, third);
+
+        //
+
 
 
 
