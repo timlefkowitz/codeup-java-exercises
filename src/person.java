657fcf09f1f625a56cbdd56735f9c3d066415c41
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-05-04 11:20:50 -0500

    Feat:updates

--- src/person.java
+++ src/person.java
@@ -1,14 +1,24 @@
 public class person {
     public static void main(String[] args) {
+    }
 
+    private String name(){
 
+        public String getName(){
 
+            //TODO: return the person's name
+        tim.firstName = "Tim";
+        tim.lastName = "Lefkowitz";
+        }
 
+        public void setName(String name){
+            //TODO: change the name property to the passed value
+        }
 
-
-
-
-
+        public void sayHello(){
+            //TODO: print a message to the console using the person's name
+            return String.format("Hello and welcome to CodeUp %s %s!", firstName, lastName)
+        }
     }
 
 }
